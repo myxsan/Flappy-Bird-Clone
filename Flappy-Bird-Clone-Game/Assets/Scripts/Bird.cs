7af5cfcfb97ebcd47d6f0ab1ea430b59bf2af530
myxsan <yksl3461@gmail.com>
2022-04-07 22:41:09 +0300

    Fixed SFX bug

--- Flappy-Bird-Clone-Game/Assets/Scripts/Bird.cs
+++ Flappy-Bird-Clone-Game/Assets/Scripts/Bird.cs
@@ -16,7 +16,7 @@ public class Bird : MonoBehaviour
     {
         gameManager = FindObjectOfType<GameManager>();
     }
-    private void Start() 
+    private void Start()
     {
         startMenuCanvas.gameObject.SetActive(true);
         gameOverCanvas.gameObject.SetActive(false);
@@ -24,40 +24,43 @@ public class Bird : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
-            if(!isPlaying)
+            if (!isPlaying)
             {
                 startMenuCanvas.gameObject.SetActive(false);
                 isPlaying = true;
                 _rigidbody.gravityScale = 0.75f;
             }
-            PlaySFX(jumpSFX);
+            if (!isDeath)
+            {
+                PlaySFX(jumpSFX);
+            }
             _rigidbody.velocity = Vector2.up * _velocity;
-        }   
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.name == "ScoreArea")
+        if (other.gameObject.name == "ScoreArea")
         {
             PlaySFX(PointSFX);
             gameManager.IncreaseScore();
-        }    
+        }
     }
-    private void OnCollisionEnter2D(Collision2D other) 
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.tag == "DeathArea")
+        if (other.gameObject.tag == "DeathArea")
         {
             PlaySFX(hitSFX);
             isDeath = true;
             Time.timeScale = 0;
             gameOverCanvas.gameObject.SetActive(true);
-        }    
+        }
     }
     public void PlaySFX(AudioClip clip)
     {
-        if(gameManager.isOpenSFX)
+        if (gameManager.isOpenSFX)
         {
             AudioSource.PlayClipAtPoint(clip, this.transform.position);
         }
